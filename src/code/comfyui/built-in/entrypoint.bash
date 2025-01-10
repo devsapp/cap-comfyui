debf577b9ae1dce30bdf3f436adf3baa96cf0e73
zijian.wzj <qiucheng.wzj@alibaba-inc.com>
2025-01-10 18:09:19 +0800

    fix bug

--- src/code/comfyui/built-in/entrypoint.bash
+++ src/code/comfyui/built-in/entrypoint.bash
@@ -5,14 +5,23 @@
 # -- models: 内置模型，/root/comfyui/models/checkpoints/xxx.safetensors -> /built-in/models/checkpoints/xxx.safetensors
 
 # - /root: 工作目录
+# -- agent: agent程序所在目录
 # -- comfyui
+# --- models: comfyui模型目录，软链接到挂载存储中，/root/comfyui/models -> /mnt/${functionName}/models
+# --- ...
 # -- venv: 依赖目录
 
 # - /mnt/${functionName}: 挂载目录，NAS or OSS
-# -- models: 用户模型，/root/comfyui/models -> /mnt/${functionName}/models
-# -- snapshots: (comfyui+venv)的快照目录，snapshot-20250101175933.tar
+# -- models: 用户模型本体，/root/comfyui/models -> /mnt/${functionName}/models
 # -- input: 输入图片
 # -- output: 输出图片
+# -- snapshots: 快照目录
+# --- 20250101-015959
+# ---- comfyui
+# ---- venv.tar
+# --- 20250102-120159
+# ---- comfyui
+# ---- venv.tar
 
 function set_start_time() {
   START_TIME=$(date '+%s.%N')
@@ -34,34 +43,25 @@ if [ "${IMAGE_TAG}" != "${IMAGE_TAG_MNT}" ]; then
   echo -n ${IMAGE_TAG} > ${MNT_DIR}/IMAGE_TAG
 fi
 
-if [ -e "${MNT_DIR}/snapshots/snapshot.tar" ]; then
-  # 优先使用用户挂载的存储中的快照
-  echo "Downloading snapshot..."
-  set_start_time
-  cp ${MNT_DIR}/snapshots/snapshot.tar .
-  show_cost_time "Downloaded snapshot"
-
-  echo "Unpacking snapshot..."
-  rm -rf comfyui venv
-  set_start_time
-  tar -xf snapshot.tar
-  show_cost_time  "Unpacked snapshot"
-  rm snapshot.tar
-fi
-
-mkdir -p ${MNT_DIR}/input
-mkdir -p ${MNT_DIR}/output
+# 初始化挂载目录
 if [ ! -e "${MNT_DIR}/models" ]; then
   cp -r ${COMFYUI_DIR}/models ${MNT_DIR}/models
 fi
 rm -rf ${COMFYUI_DIR}/models
 ln -s ${MNT_DIR}/models ${COMFYUI_DIR}/models
-ln -s ${BUILT_IN_DIR}/models/checkpoints/sd-v1-5-inpainting.ckpt ${COMFYUI_DIR}/models/checkpoints/sd-v1-5-inpainting.ckpt
+ln -sf ${BUILT_IN_DIR}/models/checkpoints/sd-v1-5-inpainting.ckpt ${COMFYUI_DIR}/models/checkpoints/sd-v1-5-inpainting.ckpt
+mkdir -p ${MNT_DIR}/input
+mkdir -p ${MNT_DIR}/output
 mkdir -p ${MNT_DIR}/snapshots
 
+# 启动(非agent模式)
 source venv/bin/activate
 echo "Using python venv, python path '$(which python)', pip path '$(which pip)'... "
 CLI_ARGS="${CLI_ARGS:---listen 0.0.0.0 --input-directory ${MNT_DIR}/input --output-directory ${MNT_DIR}/output --temp-directory ${MNT_DIR}/output}"
 EXTRA_ARGS="${EXTRA_ARGS:-}"
 export ARGS="${CLI_ARGS} ${EXTRA_ARGS}"
 python comfyui/main.py ${ARGS}
+
+# 启动(agent模式)
+source agent/venv/bin/activate
+python agent/main.py
